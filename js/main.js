daef22db55be21ef8fe31a363930e25ff89d2c0b
Denis <denisshulgin2017@yandex.ru>
2020-04-30 18:59:03 +0300

    properties

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@ var swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
     },
   });
+
+  description_button.onclick = function() {
+    if(description_button.innerHTML == "Развернуть"){
+      description_text.style.overflow = "visible";
+      description_text.style.height = "auto";
+      description_button.innerHTML = "Свернуть";
+    } else{
+      description_text.style.overflow = "hidden";
+      description_text.style.height = "50px";
+      description_button.innerHTML = "Развернуть";
+    }
+  };
+  properties_button.onclick = function() {
+    if(properties_button.innerHTML == "Развернуть"){
+      properties_text.style.overflow = "visible";
+      properties_text.style.height = "auto";
+      properties_button.innerHTML = "Свернуть";
+    } else{
+      properties_text.style.overflow = "hidden";
+      properties_text.style.height = "90px";
+      properties_button.innerHTML = "Развернуть";
+    }
+  };
