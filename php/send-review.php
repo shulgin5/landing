e776181c0a656095dc0b1e8f45b0339aa44c6794
Denis <denisshulgin2017@yandex.ru>
2020-05-04 10:50:49 +0300

    review mail

--- php/send-review.php
+++ php/send-review.php
@@ -3,17 +3,7 @@
         $client = strip_data($_POST['client']);
         $review = strip_data($_POST['review']);
         if($client!="" && $review!=""){
-            $message = "
-            <html>
-            <head>
-            </head>
-            <body>
-                <h2>Отзыв с arhfit.ru</h2>
-                <h4>$client</h4>
-                <p>$review</p>
-            </body>
-            </html>
-            ";
+            $message = $client."\n".$review;
             mail('arhfit@mail.ru', 'Отзыв', $message);
             echo 'Отзыв успешно отправлен! <br><span class="review-mark">После проверки модератором отзыв появится в блоке.</span>';
         }else{
